7b3ec2132324426f830f4b070078e386b7f13616
eVookelj <evukelj@stuy.edu>
2016-09-22 12:31:07 -0400

    Print table and have random pick fxn

---
+++ work3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from random import choice, uniform
+from flask import Flask, render_template
+import csv
+
+occs = {}
+app = Flask(__name__)
+
+def readToDict():
+    import csv
+    with open('occupations.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            job = row['Job Class']
+            percent = float(row['Percentage'])
+            if (job != 'Total'): occs[job] = percent
+
+def pickWeighted():
+    rand = uniform(0, sum(occs.itervalues())) #rand float from 0 to sum of vals
+    sigma = 0.0
+    for key, weight in occs.iteritems():
+        sigma += weight
+        if rand < sigma: return key;
+    return key
+
+def getRandOccupation():
+    readToDict()
+    return pickWeighted()
+
+@app.route("/")
+def homePage():
+    readToDict()
+    #return getRandOccupation()
+    return render_template('main.html', title="Occupations", table=occs)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
