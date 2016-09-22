9088d9817fd9d4b94a120cdc42a2021b86cf0706
eVookelj <evukelj@stuy.edu>
2016-09-22 19:47:27 -0400

    Show random pick and add formatting

--- work3.py
+++ work3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from random import choice, uniform
+from random import uniform
 from flask import Flask, render_template
 import csv
 
@@ -30,9 +30,8 @@ def getRandOccupation():
 
 @app.route("/")
 def homePage():
-    readToDict()
-    #return getRandOccupation()
-    return render_template('main.html', title="Occupations", table=occs)
+    randomOcc = getRandOccupation()
+    return render_template('main.html', title="Occupations", job = randomOcc, table=occs)
 
 if __name__ == "__main__":
     app.debug = True
