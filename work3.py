c38deda9ab97b9ef0b6ad655d4b47394036b8a86
eVookelj <evukelj@stuy.edu>
2016-09-23 12:27:09 -0400

    Make code work with links

--- work3.py
+++ work3.py
@@ -5,21 +5,25 @@ from flask import Flask, render_template
 import csv
 
 occs = {}
+newDict = {}
 app = Flask(__name__)
 
 def readToDict():
     import csv
-    with open('occupations.csv') as csvfile:
+    with open('occUpdate.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             job = row['Job Class']
             percent = float(row['Percentage'])
-            if (job != 'Total'): occs[job] = percent
+            link = row['Link']
+            if (job != 'Total'): occs[job] = [percent, link]
 
 def pickWeighted():
-    rand = uniform(0, sum(occs.itervalues())) #rand float from 0 to sum of vals
+    for occ in occs:
+        newDict[occ] = occs[occ][0]
+    rand = uniform(0, sum(newDict.itervalues())) #rand float from 0 to sum of vals
     sigma = 0.0
-    for key, weight in occs.iteritems():
+    for key, weight in newDict.iteritems():
         sigma += weight
         if rand < sigma: return key;
     return key
@@ -31,7 +35,7 @@ def getRandOccupation():
 @app.route("/")
 def homePage():
     randomOcc = getRandOccupation()
-    return render_template('main.html', title="Occupations", job = randomOcc, table=occs)
+    return render_template('main.html', title="Occupations", link=occs[randomOcc][1], job = randomOcc, table=newDict)
 
 if __name__ == "__main__":
     app.debug = True
